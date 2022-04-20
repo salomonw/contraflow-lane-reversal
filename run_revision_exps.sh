fcca0423a9efa5814b07301a8be8ec7f74c0b234
salomonw <salomonw@bu.edu>
2022-04-20 11:32:54 -0400

    loading gurobi module for server

--- run_revision_exps.sh
+++ run_revision_exps.sh
@@ -7,6 +7,9 @@
 #$ -pe omp 8
 #$ -l h_rt=17:59:00
 
+# Load modules
+module load gurobi
+
 # Run programs
 python3 -m experiments.test_max_reversals test_9 1
 python3 -m experiments.test_max_reversals EMA_mid 2
